9781bf9e27374fc1f71d1a701cb1538c9a774ee9
messiasGeovani <messias.geovani.lima@gmail.com>
2019-11-04 18:29:53 -0300

    model config

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -15,12 +15,43 @@ const User = new Schema({
         type: String,
         trim: true,
         required: true
-    }
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]    
 })
 
 // hashing password
-User.pre('save', function(next) {
-    this.password = bcrypt.hashSync(this.password, 8)
+User.pre('save', async function(next) {
+    // hashing the password
+    const user = this
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+    next()
 })
+
+// generate token method
+User.methods.generateToken = async function() {
+    // generating the token
+    const user = this
+    const token = jwt.sign({_id: user._id}, process.env.JWT_KEY)
+    user.tokens = user.tokens.concat({token})
+    await user.save()
+    return token
+}
+
+// find by token method
+User.statics.findByCredentials = async (email, password) => {
+    // searching data
+    const user = await User.findOne({ email })
+    if (!user) {
+        throw new Error({ Error: 'Invalid login credentials' })
+    }
+    return user
+}
 
 exports.User = mongoose.model('User', User)
