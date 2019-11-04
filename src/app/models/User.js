a502fe6cffabad756623c0254536840b6824e638
messiasGeovani <messias.geovani.lima@gmail.com>
2019-11-04 14:14:28 -0300

    generate token function config

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,9 +1,21 @@
 const Schema = require('mongoose').Schema
 const ObjectId = Schema.Types.ObjectId
 
+// jwt import
+const jwt = require('jsonwebtoken')
+
 // user model
 exports.User = new Schema({
     _id: ObjectId,
     email: String,
     password: String
 })
+
+// generate token function
+User.prototype.generateToken = function() {
+    return jwt.sign(
+        {
+            id: this.id
+        }
+    )
+}
