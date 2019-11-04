4dd1118d9186fa7b0c5d35432eb224ab3c7651c6
messiasGeovani <messias.geovani.lima@gmail.com>
2019-11-04 14:02:50 -0300

    session controller file

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,32 @@
+const User = require('../models').User
+
+/**
+ * Session Controller
+ */
+exports.SessionControllers = new class {
+    async store() {
+        const { email, password } = req.body
+
+        // getting a data
+        const user = User.findOne({ where: { email } })
+
+        // checking the user
+        if (!user) {
+            res.status(404).json({
+                message: 'User not found'
+            })
+        }
+
+        // checking the password
+        if (!password) {
+            resstatus(404).json({
+                message: 'Invalid password'
+            })
+        }
+
+        return res.json({
+            user,
+            token: user.generateToken()
+        })
+    }
+}
