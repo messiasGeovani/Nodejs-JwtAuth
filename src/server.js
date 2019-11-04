e09565100a64b9892878563e1515f5f598c4032d
messiasGeovani <messias.geovani.lima@gmail.com>
2019-11-04 18:05:12 -0300

    database config

--- src/server.js
+++ src/server.js
@@ -1,5 +1,15 @@
 const app = require('./').App
 
+const mongoose = require('mongoose')
+
+mongoose.connect(
+    'mongodb+srv://dev:1234@cluster0-jvwp7.mongodb.net/jwt?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+)
+
 // starting the server
 app.listen(3000)
 console.log('Server is running at 3000')
